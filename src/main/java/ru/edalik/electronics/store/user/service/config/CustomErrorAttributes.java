797c53427bdf45d494accebab36e973b940fc1e9
Edalik <www.ayakubov@yandex.ru>
2025-03-10 23:00:06 +0300

    Exception handling

---
+++ src/main/java/ru/edalik/electronics/store/user/service/config/CustomErrorAttributes.java
@@ -0,0 +1,24 @@
+package ru.edalik.electronics.store.user.service.config;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        return super.getErrorAttributes(
+            webRequest,
+            options.excluding(
+                ErrorAttributeOptions.Include.STACK_TRACE,
+                ErrorAttributeOptions.Include.MESSAGE
+            )
+        );
+    }
+
+}
